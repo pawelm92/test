727f0722a3ebf155f562da7b1d3de74838082b61
pmielnik92 <pmielnik92@gmail.com>
2018-03-23 13:31:54 +0100

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,7 @@
 public class HelloWorld {
 
-    no hej
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
 }
